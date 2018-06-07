aeb525ca3900c0152f93cc6c442f230cd09ed43f
heikal-b <heikal93@gmail.com>
2018-06-06 17:43:39 -0700

    Added another method and docstrings

--- measurements.py
+++ measurements.py
@@ -4,19 +4,52 @@ import nltk
 
 
 def participle_adj(name):
+    """
+    Tell if a word is a participle adjective
+    :param name: The target word
+    :return: True if the word is a participle adjective
+    """
     return re.search('-?.*(ed)+$', name) is not None
 
 
 def has_apostrophe(name):
+    """
+    Tell if a word contains an apostrophe
+    :param name: The target word
+    :return: True if the word contains an apostrophe
+    """
     return re.search("('s|s')+$", name) is not None
 
 
 def num_mods(name):
+    """
+    Return the number of words in a string
+    :param name: The target string
+    :return: Number of words in a string
+    """
     return len(name.split())
 
 
 def mod_pos(name):
-    return nltk.pos_tag(name.split())
+    """
+    Return the list of POS in a name
+    :param name: The target name
+    :return: List of POS tags
+    """
+    name = name.lower()
+    tup_list = nltk.pos_tag(name.split())
+    return [p[1] for p in tup_list]
+
+
+def embeddedness(target_list, compared_list):
+    """
+    Measure the embedddedness of one list within another; embeddedness of A in B = #(A int B)/#A
+    :param target_list: The target list
+    :param compared_list: The list to be compared with
+    :return: Embeddedness score
+    """
+    intersection = [e for e in target_list if e in compared_list]
+    return len(intersection)/len(target_list)
 
 
 def main():
@@ -31,7 +64,11 @@ def main():
     print(has_apostrophe(downy_wp.mod_name))
 
     gb_heron = NamedBird('Great Blue Heron', 'Ardea herodias', 'Pelecaniformes', 'Ardeidae')
-    print(mod_pos(gb_heron.mod_name))
+    print(mod_pos(gb_heron.full_name))
+
+    list1 = ['Blue', 'Mississippi', 'Atlantic', 'Common', 'Northern']
+    list2 = ['Common', 'Northern', 'Bald']
+    print(embeddedness(list2, list1))
 
 
 if __name__ == '__main__':
